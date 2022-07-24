e6dda5b7a6a69cf75467e03a2ea02591444d54df
zagubygorilka <zagubygorilka@gamil.com>
2022-07-24 13:53:38 +0300

    new

---
+++ JS homework/homework1/hw1.js
@@ -0,0 +1,64 @@
+let one;
+one = `hello`;
+console
+console.log(one);
+let two;
+two = `owu`;
+alert(two);
+let three;
+three = `com`;
+document.write(three);
+let four;
+four = `ua`;
+console.log(four);
+let five;
+five = 1;
+alert(five);
+let six;
+six = 10;
+document.write(six);
+let seven;
+seven = -999
+console.log(seven);
+let eight;
+eight = 123;
+alert(eight);
+let nine;
+nine = 3.14;
+document.write(nine);
+let ten;
+ten = 2.7;
+console.log(ten);
+let eleven;
+eleven = 16;
+alert(eleven);
+let twelve;
+twelve = true;
+document.write(twelve);
+let thirteen;
+thirteen = false;
+console.log(thirteen);
+
+
+let firstName;
+firstName = 'Загубигорілка ';
+let middleName;
+middleName = `Володимир `;
+let lastName;
+lastName = `Геннадійович`;
+console.log(firstName + middleName + lastName);
+
+
+let a;
+a = 100;
+let b;
+b = `100`;
+let c;
+c = true;
+console.log(typeof a, b, c);
+
+
+let name= prompt('Вкажіть прізвище?');
+let secondName= prompt(`Ім'я?`);
+let age= Number(prompt(`Скільки вам років?`));
+alert(name + ` ` + secondName + `, ` + age);
